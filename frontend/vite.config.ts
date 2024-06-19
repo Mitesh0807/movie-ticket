c3d85dc7111962f98dc617d2c3377ae8b42fbf69
Mitesh0807 <miteshsavaliya7@gmail.com>
2024-06-19 20:16:43 +0530

    docker setup

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import path from 'path'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5173,
+    host: true,
+    watch: {
+      usePolling: true,
+    },
+  },
+  preview: {
+    port: 5173,
+    host: true,
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
